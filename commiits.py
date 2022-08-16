0aa5848015e21ad846d5e5a8c7509ee544d28cb4
Shiroth-blip <83892990+Shiroth-blip@users.noreply.github.com>
2022-08-16 12:33:45 -0300

    Add files via upload

---
+++ commiits.py
@@ -0,0 +1,11 @@
+str = open("logmozilla.txt","r").read()
+lista=str.split("commit")
+print(lista[:2])
+print(len(lista)-1)
+
+archivo_texto = open("commits.txt", "w")
+
+for commit in lista:
+    archivo_texto.write(commit + "\n")
+    
+archivo_texto.close()
